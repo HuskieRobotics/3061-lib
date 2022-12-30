48f78853a46d0ae4056253bd4bdaba4d71daa0d2
Geoff Schmit <gcschmit@gmail.com>
2022-12-30 12:46:35 -0600

    add PathPlanner 2023 example; add drive velocity tuning auto mode

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.team3061.swerve.SwerveModuleConstants;
+import java.util.HashMap;
 
 public final class DrivetrainConstants {
 
@@ -100,19 +102,21 @@ public final class DrivetrainConstants {
 
   public static final int TIMEOUT_MS = 30;
 
-  public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 3;
-  public static final double AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 3;
+  public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 2.0;
+  public static final double AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2.0;
   public static final double AUTO_MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = 2.0 * Math.PI;
   public static final double AUTO_MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED = 2.0 * Math.PI;
 
   // FIXME: tune PID values for auto paths
 
-  public static final double AUTO_DRIVE_P_CONTROLLER = 2.2956;
+  public static final double AUTO_DRIVE_P_CONTROLLER = 6.0;
   public static final double AUTO_DRIVE_I_CONTROLLER = 0.0;
   public static final double AUTO_DRIVE_D_CONTROLLER = 0.0;
-  public static final double AUTO_TURN_P_CONTROLLER = 4.9;
+  public static final double AUTO_TURN_P_CONTROLLER = 10.0;
   public static final double AUTO_TURN_I_CONTROLLER = 0.0;
   public static final double AUTO_TURN_D_CONTROLLER = 0.0;
+
+  public static final HashMap<String, Command> AUTO_EVENT_MAP = new HashMap<>();
 
   public static final double DEADBAND = 0.1;
 }
