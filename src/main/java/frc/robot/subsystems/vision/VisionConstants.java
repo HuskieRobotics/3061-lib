c39ee27dca5da95a67c87ed6e25b99c84638bba0
cyandev <djs.bitcoin@gmail.com>
2023-01-05 16:10:56 -0500

    add note to change robot to camera

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -11,10 +10,10 @@ public final class VisionConstants {
     private VisionConstants() {
         throw new IllegalStateException(CONSTRUCTOR_EXCEPTION);
     }
-    
+     
     public static String APRILTAG_FIELD_LAYOUT_PATH = "";
     public static double MAXIMUM_AMBIGUITY = .2; 
-    public static Transform3d ROBOT_TO_CAMERA = new Transform3d(
+    public static Transform3d ROBOT_TO_CAMERA = new Transform3d( //FIXME update this with the real transform
         new Translation3d(0,0,0), 
         new Rotation3d(0,0,0)
     );
