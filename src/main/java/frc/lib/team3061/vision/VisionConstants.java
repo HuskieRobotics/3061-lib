e2d4546e06404a6f7fd2a9f5027478913740262b
Geoff Schmit <gcschmit@gmail.com>
2023-01-05 15:35:15 -0600

    add back comment

--- src/main/java/frc/lib/team3061/vision/VisionConstants.java
+++ src/main/java/frc/lib/team3061/vision/VisionConstants.java
@@ -14,5 +14,7 @@ public final class VisionConstants {
   public static final String APRILTAG_FIELD_LAYOUT_PATH = "";
   public static final double MAXIMUM_AMBIGUITY = .2;
   public static final Transform3d ROBOT_TO_CAMERA =
-      new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
+      new Transform3d(
+          new Translation3d(0, 0, 0),
+          new Rotation3d(0, 0, 0)); // FIXME: update this with the real transform
 }
