7892f35a9c87888357f52fd4089b90ee1376ff3a
Wyatt Woker <ambase6@gmail.com>
2023-01-11 20:34:57 -0600

    made a new branch for a git issue

---
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainUtility.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.drivetrain;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
+
+
+
+public class DrivetrainUtility{
+
+    private ChassisSpeeds 
+
+
+}
