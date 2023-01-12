ae0fe52f55da86d7cc2cd97d3587122a396adc08
Wyatt Woker <ambase6@gmail.com>
2023-01-12 17:55:42 -0600

    built a method and prepped for more code

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainUtility.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainUtility.java
@@ -9,7 +9,24 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class DrivetrainUtility{
 
-    private ChassisSpeeds 
+    private static Translation2d mostRecentRequestedVelocity = new Translation2d();
+    private static double mostRecentRequestedRotation = 0;
+    private static double mostRecentLoopTime = 0;
+    /**
+     * 
+     * @param desiredVelocity the desired restricted velocity
+     * @param currentVSpeeds    the current velocity of the chassis
+     * @param accelerationLimit  the limit for how fast the robot can accelerate. Prevents issues such as faceplanting
+     * @param rotatingAccelerationLimit the limit for how fast the robot can turn
+     * @param gyroAngle the current gyro angle
+     * @return
+     */
+    public static Translation2d limitAcceleration(ChassisSpeeds desiredVelocity, ChassisSpeeds currentVSpeeds, Double accelerationLimit, Double rotatingAccelerationLimit, Rotation2d gyroAngle){
+        ChassisSpeeds commandedVelocity;
+        
 
 
-}
+        Translation2d velocity = ChassisSpeeds.fromFieldRelativeSpeeds(new Translation2d(
+                        commandedVelocity.vxMetersPerSecond,
+                        commandedVelocity.vyMetersPerSecond),
+                gyroAngle);
