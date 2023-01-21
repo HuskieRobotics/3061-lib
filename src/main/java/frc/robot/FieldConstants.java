90308dead2c6f86444ea47a16771afbefbb3d5e0
SelinaLingY <56369463+SelinaLingY@users.noreply.github.com>
2023-01-21 14:54:17 -0600

    updates to the FieldConstants

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -13,24 +13,24 @@ public final class FieldConstants{
     // Current FieldConstants are for the FRC 2023 Game "Charged Up"
     //NOTE TO SELF: All constants to be accessed globally should be initialized using (public static)
 
-    private static final Translation2d COMMUNITY_POINT_1 = new Translation2d(0,0);
-    private static final Translation2d COMMUNITY_POINT_2 = new Translation2d(0,5.49);
-    private static final Translation2d COMMUNITY_POINT_3 = new Translation2d(3,5.49);
-    private static final Translation2d COMMUNITY_POINT_4 = new Translation2d(3.36,5.49);
-    private static final Translation2d COMMUNITY_POINT_5 = new Translation2d(3.36,3.98);
-    private static final Translation2d COMMUNITY_POINT_6 = new Translation2d(3,3.98);
-    private static final Translation2d COMMUNITY_POINT_7 = new Translation2d(3,1.51);
-    private static final Translation2d COMMUNITY_POINT_8 = new Translation2d(4.91, 1.51);
-    private static final Translation2d COMMUNITY_POINT_9 = new Translation2d(4.91, 0);
-    private static final Translation2d COMMUNITY_POINT_10 = new Translation2d(3,0);
+    public static final Translation2d COMMUNITY_POINT_1 = new Translation2d(0,0);
+    public static final Translation2d COMMUNITY_POINT_2 = new Translation2d(0,5.49);
+    public static final Translation2d COMMUNITY_POINT_3 = new Translation2d(3,5.49);
+    public static final Translation2d COMMUNITY_POINT_4 = new Translation2d(3.36,5.49);
+    public static final Translation2d COMMUNITY_POINT_5 = new Translation2d(3.36,3.98);
+    public static final Translation2d COMMUNITY_POINT_6 = new Translation2d(3,3.98);
+    public static final Translation2d COMMUNITY_POINT_7 = new Translation2d(3,1.51);
+    public static final Translation2d COMMUNITY_POINT_8 = new Translation2d(4.91, 1.51);
+    public static final Translation2d COMMUNITY_POINT_9 = new Translation2d(4.91, 0);
+    public static final Translation2d COMMUNITY_POINT_10 = new Translation2d(3,0);
 
-    private static final Translation2d[] COMMUNITY_REGION_POINTS_1 = new Translation2d[] {
+    public static final Translation2d[] COMMUNITY_REGION_POINTS_1 = new Translation2d[] {
         COMMUNITY_POINT_1, COMMUNITY_POINT_2, COMMUNITY_POINT_3, COMMUNITY_POINT_10
     };
-    private static final Translation2d[] COMMUNITY_REGION_POINTS_2 = new Translation2d[] {
+    public static final Translation2d[] COMMUNITY_REGION_POINTS_2 = new Translation2d[] {
         COMMUNITY_POINT_6, COMMUNITY_POINT_3, COMMUNITY_POINT_4, COMMUNITY_POINT_5
     };
-    private static final Translation2d[] COMMUNITY_REGION_POINTS_3 = new Translation2d[] {
+    public static final Translation2d[] COMMUNITY_REGION_POINTS_3 = new Translation2d[] {
         COMMUNITY_POINT_10, COMMUNITY_POINT_7, COMMUNITY_POINT_8, COMMUNITY_POINT_9
     };
 
@@ -43,15 +43,15 @@ public final class FieldConstants{
     );
 
     //TODO: Replace placeholder X values for each grid node location
-    public final static Pose2d GRID_1_NODE_1 = new Pose2d(2, 0.51, new Rotation2d(Math.PI));    //should be 180 in radians
-    public final static Pose2d GRID_1_NODE_2 = new Pose2d(2, 1.05, new Rotation2d(Math.PI));
-    public final static Pose2d GRID_1_NODE_3 = new Pose2d(2, 1.63, new Rotation2d(Math.PI));
+    public final static Pose2d GRID_1_NODE_1 = new Pose2d(2, 0.51, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_1_NODE_2 = new Pose2d(2, 1.05, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_1_NODE_3 = new Pose2d(2, 1.63, Rotation2d.fromDegrees(180));
 
-    public final static Pose2d GRID_2_NODE_1 = new Pose2d(2, 2.19, new Rotation2d(Math.PI));
-    public final static Pose2d GRID_2_NODE_2 = new Pose2d(2, 2.75, new Rotation2d(Math.PI));
-    public final static Pose2d GRID_2_NODE_3 = new Pose2d(2, 3.31, new Rotation2d(Math.PI));
+    public final static Pose2d GRID_2_NODE_1 = new Pose2d(2, 2.19, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_2_NODE_2 = new Pose2d(2, 2.75, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_2_NODE_3 = new Pose2d(2, 3.31, Rotation2d.fromDegrees(180));
 
-    public final static Pose2d GRID_3_NODE_1 = new Pose2d(2, 3.87, new Rotation2d(Math.PI));
-    public final static Pose2d GRID_3_NODE_2 = new Pose2d(2, 4.42, new Rotation2d(Math.PI));
-    public final static Pose2d GRID_3_NODE_3 = new Pose2d(2, 4.98, new Rotation2d(Math.PI));
+    public final static Pose2d GRID_3_NODE_1 = new Pose2d(2, 3.87, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_3_NODE_2 = new Pose2d(2, 4.42, Rotation2d.fromDegrees(180));
+    public final static Pose2d GRID_3_NODE_3 = new Pose2d(2, 4.98, Rotation2d.fromDegrees(180));
 }
