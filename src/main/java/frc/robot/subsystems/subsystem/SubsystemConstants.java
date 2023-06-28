007f394b96c2da24784ab6b00c1651387c7b7e21
Geoff Schmit <gcschmit@gmail.com>
2023-06-28 12:12:37 -0500

    port example subsystem to Phoenix6

--- src/main/java/frc/robot/subsystems/subsystem/SubsystemConstants.java
+++ src/main/java/frc/robot/subsystems/subsystem/SubsystemConstants.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.subsystem;
 
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-
 public class SubsystemConstants {
 
   private static final String CONSTRUCTOR_EXCEPTION = "constant class";
@@ -18,18 +16,15 @@ public class SubsystemConstants {
   public static final double GEAR_RATIO = 100.0;
   public static final boolean MOTOR_INVERTED = false;
 
-  public static final double POSITION_PID_F = 0.0;
   public static final double POSITION_PID_P = 0.0;
   public static final double POSITION_PID_I = 0;
   public static final double POSITION_PID_D = 0;
-  public static final double POSITION_PID_I_ZONE = 0;
   public static final double POSITION_PID_PEAK_OUTPUT = 1.0;
   public static final double POSITION_FEEDFORWARD = 0;
 
-  public static final double CURRENT_LIMIT = 40;
-
-  public static final StatorCurrentLimitConfiguration CURRENT_LIMIT_CONFIG =
-      new StatorCurrentLimitConfiguration(true, CURRENT_LIMIT, 50, 0.5);
+  public static final double CONTINUOUS_CURRENT_LIMIT = 40;
+  public static final double PEAK_CURRENT_LIMIT = 50;
+  public static final double PEAK_CURRENT_DURATION = 0.5;
 
   public static final int TIMEOUT_MS = 30;
   public static final int SLOT_INDEX = 0;
