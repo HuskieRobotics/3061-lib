a7f5b7d1942de86bdbcaba4929b1efe3a7bccf93
Geoff Schmit <gcschmit@gmail.com>
2023-09-09 14:12:25 -0500

    Phoenix v6 (#51)
    * add getPhoenix6Licensed method to RobotConfig to determine if a Phoenix 6 Pro license is installed for CTRE devices
    * remove getSwerveAngleKF and getSwerveDriveKF methods from RobotConfig as they are no longer relevant with Phoenix 6
    * add setYaw method to GyroIO since this can now be done synchronously; as a result, gyroOffset in Drivetrain has been removed
    * replaced GyroIOPigeon2 with GyroIOPigeon2Phoenix6 to port to Phoenix 6
    * refactor Conversions to port to Phoenix 6 (supports units used by Phoenix 6)
    * replace custom swerve module optimize method with SwerveModuleState.optimize which is compatible with Phoenix 6 behavior
    * add closed-loop errors to SwerveModuleIOInputs to facilitate tuning (fixes log PID controller error #44)
    * change current and temperature inputs to single values instead of arrays
    * add stator and supply current to SwerveModuleIOInputs (previously was just stator)
    * replace SwerveModuleIOTalonFX with SwerveModuleIOTalonFXPhoenix6 to port to Phoenix 6. Key features:
    * FOC enabled if Phoenix 6 is licensed
    * all odometry-related signals are synchronized if Phoenix 6 is licensed (includes gyro and is managed in Drivetrain)
    * leverages high-fidelity simulation of CTRE hardware devices
    * incorporate 254/ fix for swerve drift while rotating; see convertFromDiscreteChassisSpeedsToContinuous method in Drivetrain
    * add logging of autonmous paths to AdvantageScope to assist with tuning (fixes log PID controller error #44)
    * use actual timestamp when updating pose estimator instead of time from AdvantageKit
    * log measured swerve module states in addition to serve module state setpoints such that both can be displayed in AdvantageScope
    * port SubsystemIOTalonFX to Phoenix 6
    * increase DRIVE_CONTINUOUS_CURRENT_LIMIT for swerve drive motor to 50 amps
    * leverage Phoenix 6 continuous wrap feature
    * use Fused CANcoder as feedback sensor for swerve angle control

--- src/main/java/frc/robot/subsystems/subsystem/SubsystemConstants.java
+++ src/main/java/frc/robot/subsystems/subsystem/SubsystemConstants.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.subsystem;
 
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-
 public class SubsystemConstants {
 
   private static final String CONSTRUCTOR_EXCEPTION = "constant class";
@@ -10,26 +8,23 @@ public class SubsystemConstants {
     throw new IllegalStateException(CONSTRUCTOR_EXCEPTION);
   }
 
-  public static final boolean DEBUGGING = true;
-  public static final boolean TESTING = true;
+  public static final boolean DEBUGGING = false;
+  public static final boolean TESTING = false;
   public static final String SUBSYSTEM_NAME = "Subsystem";
 
-  public static final int MOTOR_CAN_ID = 6;
+  public static final int MOTOR_CAN_ID = 20;
   public static final double GEAR_RATIO = 100.0;
   public static final boolean MOTOR_INVERTED = false;
 
-  public static final double POSITION_PID_F = 0.0;
   public static final double POSITION_PID_P = 0.0;
   public static final double POSITION_PID_I = 0;
   public static final double POSITION_PID_D = 0;
-  public static final double POSITION_PID_I_ZONE = 0;
   public static final double POSITION_PID_PEAK_OUTPUT = 1.0;
   public static final double POSITION_FEEDFORWARD = 0;
 
-  public static final double CURRENT_LIMIT = 40;
-
-  public static final StatorCurrentLimitConfiguration CURRENT_LIMIT_CONFIG =
-      new StatorCurrentLimitConfiguration(true, CURRENT_LIMIT, 50, 0.5);
+  public static final double CONTINUOUS_CURRENT_LIMIT = 40;
+  public static final double PEAK_CURRENT_LIMIT = 50;
+  public static final double PEAK_CURRENT_DURATION = 0.5;
 
   public static final int TIMEOUT_MS = 30;
   public static final int SLOT_INDEX = 0;
