4058f2df885e8923d63c56d9959079b78940b06e
Geoff Schmit <gcschmit@gmail.com>
2023-11-06 15:51:04 -0600

    check for unreachable CTRE devices and report faults

--- src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingPhoenixMotor.java
+++ src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingPhoenixMotor.java
@@ -1,5 +1,7 @@
 package frc.lib.team3015.subsystem.selfcheck;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.lib.team3015.subsystem.SubsystemFault;
 import java.util.ArrayList;
@@ -8,49 +10,56 @@ import java.util.List;
 public class SelfCheckingPhoenixMotor implements SelfChecking {
   private final String label;
   private final TalonFX motor;
+  private StatusSignal<Double> statusSignal;
 
   public SelfCheckingPhoenixMotor(String label, TalonFX motor) {
     this.label = label;
     this.motor = motor;
+    this.statusSignal = this.motor.getSupplyVoltage();
   }
 
   @Override
   public List<SubsystemFault> checkForFaults() {
     List<SubsystemFault> faults = new ArrayList<>();
 
-    if (motor.getFault_Hardware().getValue()) {
+    if (motor.getFault_Hardware().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Hardware failure detected", label)));
     }
-    if (motor.getFault_BootDuringEnable().getValue()) {
+    if (motor.getFault_BootDuringEnable().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Device booted while enabled", label)));
     }
-    if (motor.getFault_DeviceTemp().getValue()) {
+    if (motor.getFault_DeviceTemp().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(
               String.format("[%s]: Device temperature exceeded limit", label), true));
     }
-    if (motor.getFault_FusedSensorOutOfSync().getValue()) {
+    if (motor.getFault_FusedSensorOutOfSync().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Remote sensor is out of sync", label)));
     }
-    if (motor.getFault_MissingRemoteSensor().getValue()) {
+    if (motor.getFault_MissingRemoteSensor().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Remote sensor is missing", label)));
     }
-    if (motor.getFault_OverSupplyV().getValue()) {
+    if (motor.getFault_OverSupplyV().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Supply voltage exceeded limit", label)));
     }
-    if (motor.getFault_ProcTemp().getValue()) {
+    if (motor.getFault_ProcTemp().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(String.format("[%s]: Processor temperature exceeded limit", label)));
     }
-    if (motor.getFault_Undervoltage().getValue()) {
+    if (motor.getFault_Undervoltage().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(String.format("[%s]: Device supply voltage near brownout", label)));
     }
-    if (motor.getFault_UnlicensedFeatureInUse().getValue()) {
+    if (motor.getFault_UnlicensedFeatureInUse().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Unlicensed feature in use", label)));
     }
-    if (motor.getFault_UnstableSupplyV().getValue()) {
+    if (motor.getFault_UnstableSupplyV().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Supply voltage is unstable", label)));
+    }
+
+    this.statusSignal.refresh();
+    if (this.statusSignal.getError() != StatusCode.OK) {
+      faults.add(new SubsystemFault(String.format("[%s]: device is unreachable", label)));
     }
 
     return faults;
--- src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingPigeon2.java
+++ src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingPigeon2.java
@@ -1,5 +1,7 @@
 package frc.lib.team3015.subsystem.selfcheck;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import frc.lib.team3015.subsystem.SubsystemFault;
 import java.util.ArrayList;
@@ -8,64 +10,71 @@ import java.util.List;
 public class SelfCheckingPigeon2 implements SelfChecking {
   private final String label;
   private final Pigeon2 pigeon;
+  private StatusSignal<Double> statusSignal;
 
   public SelfCheckingPigeon2(String label, Pigeon2 pigeon) {
     this.label = label;
     this.pigeon = pigeon;
+    this.statusSignal = this.pigeon.getSupplyVoltage();
   }
 
   @Override
   public List<SubsystemFault> checkForFaults() {
     List<SubsystemFault> faults = new ArrayList<>();
 
-    if (pigeon.getFault_Hardware().getValue()) {
+    if (pigeon.getFault_Hardware().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Hardware fault detected", label)));
     }
-    if (pigeon.getFault_BootDuringEnable().getValue()) {
+    if (pigeon.getFault_BootDuringEnable().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Device booted while enabled", label)));
     }
-    if (pigeon.getFault_BootIntoMotion().getValue()) {
+    if (pigeon.getFault_BootIntoMotion().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Device booted while in motion", label)));
     }
-    if (pigeon.getFault_BootupGyroscope().getValue()) {
+    if (pigeon.getFault_BootupGyroscope().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Gyro fault detected", label)));
     }
-    if (pigeon.getFault_BootupAccelerometer().getValue()) {
+    if (pigeon.getFault_BootupAccelerometer().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Accelerometer fault detected", label)));
     }
-    if (pigeon.getFault_BootupMagnetometer().getValue()) {
+    if (pigeon.getFault_BootupMagnetometer().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Magnetometer fault detected", label)));
     }
-    if (pigeon.getFault_DataAcquiredLate().getValue()) {
+    if (pigeon.getFault_DataAcquiredLate().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(
               String.format("[%s]: Motion stack data acquisition slower than expected", label)));
     }
-    if (pigeon.getFault_Hardware().getValue()) {
+    if (pigeon.getFault_Hardware().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Hardware failure", label)));
     }
-    if (pigeon.getFault_LoopTimeSlow().getValue()) {
+    if (pigeon.getFault_LoopTimeSlow().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(
               String.format("[%s]: Motion stack loop time was slower than expected", label)));
     }
-    if (pigeon.getFault_SaturatedAccelometer().getValue()) {
+    if (pigeon.getFault_SaturatedAccelometer().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(String.format("[%s]: Accelerometer values are saturated", label)));
     }
-    if (pigeon.getFault_SaturatedGyrosscope().getValue()) {
+    if (pigeon.getFault_SaturatedGyrosscope().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Gyro values are saturated", label)));
     }
-    if (pigeon.getFault_SaturatedMagneter().getValue()) {
+    if (pigeon.getFault_SaturatedMagneter().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(String.format("[%s]: Magnetometer values are saturated", label)));
     }
-    if (pigeon.getFault_Undervoltage().getValue()) {
+    if (pigeon.getFault_Undervoltage().getValue() == Boolean.TRUE) {
       faults.add(
           new SubsystemFault(String.format("[%s]: Device supply voltage near brownout", label)));
     }
-    if (pigeon.getFault_UnlicensedFeatureInUse().getValue()) {
+    if (pigeon.getFault_UnlicensedFeatureInUse().getValue() == Boolean.TRUE) {
       faults.add(new SubsystemFault(String.format("[%s]: Unlicensed feature in use", label)));
+    }
+
+    this.statusSignal.refresh();
+    if (this.statusSignal.getError() != StatusCode.OK) {
+      faults.add(new SubsystemFault(String.format("[%s]: device is unreachable", label)));
     }
 
     return faults;
