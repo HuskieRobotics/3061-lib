52455cae53455a2dec0aefb593c18e95adf16f34
Geoff Schmit <gcschmit@gmail.com>
2023-11-06 18:07:03 -0600

    add PathPlanner/AdvantageKit fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import com.pathplanner.lib.pathfinding.Pathfinding;
+
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -40,6 +42,9 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotInit() {
+    // DO THIS FIRST
+    Pathfinding.setPathfinder(new LocalADStarAK());
+
     final String GIT_DIRTY = "GitDirty";
 
     // from AdvantageKit Robot Configuration docs
