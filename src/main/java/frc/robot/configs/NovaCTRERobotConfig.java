2366dd09ed06633b706cf3a2974cf8b10e83ced6
Geoff Schmit <gcschmit@gmail.com>
2023-11-26 19:30:11 -0600

    restore kS, kV, kA values

--- src/main/java/frc/robot/configs/NovaCTRERobotConfig.java
+++ src/main/java/frc/robot/configs/NovaCTRERobotConfig.java
@@ -45,18 +45,18 @@ public class NovaCTRERobotConfig extends RobotConfig {
   private static final double ANGLE_KI = 0.0;
   private static final double ANGLE_KD = 0.05;
 
-  private static final double ANGLE_KS = 0.0; // 0.1891233333;
-  private static final double ANGLE_KV = 0.0; // 0.4399866667;
-  private static final double ANGLE_KA = 0.0; // 0.001663333333;
+  private static final double ANGLE_KS = 0.1891233333;
+  private static final double ANGLE_KV = 0.4399866667;
+  private static final double ANGLE_KA = 0.001663333333;
 
   /* Drive Motor PID Values */
   private static final double DRIVE_KP = 3.0;
   private static final double DRIVE_KI = 0.0;
   private static final double DRIVE_KD = 0.0;
 
-  private static final double DRIVE_KS = 0.0; // 0.4004375;
-  private static final double DRIVE_KV = 0.0; // 2.7637325;
-  private static final double DRIVE_KA = 0.0; // 0.0139575;
+  private static final double DRIVE_KS = 0.4004375;
+  private static final double DRIVE_KV = 2.7637325;
+  private static final double DRIVE_KA = 0.0139575;
 
   private static final SwerveType SWERVE_TYPE = SwerveType.MK4I;
 
