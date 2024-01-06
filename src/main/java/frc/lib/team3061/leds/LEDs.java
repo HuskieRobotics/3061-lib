1fc5e32de267e59a0ff47f9858a475554dddc519
Geoff Schmit <gcschmit@gmail.com>
2024-01-05 20:43:48 -0600

    demote hardware responsibility to add RIO support

--- src/main/java/frc/lib/team3061/leds/LEDs.java
+++ src/main/java/frc/lib/team3061/leds/LEDs.java
@@ -7,29 +7,28 @@
 
 package frc.lib.team3061.leds;
 
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdle.LEDStripType;
-import com.ctre.phoenix.led.CANdle.VBatOutputMode;
-import com.ctre.phoenix.led.CANdleConfiguration;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.team3061.RobotConfig;
 import java.util.List;
 
 @java.lang.SuppressWarnings({"java:S6548"})
-public class LEDs extends SubsystemBase {
+public abstract class LEDs extends SubsystemBase {
 
   private static LEDs instance;
 
   public static LEDs getInstance() {
     if (instance == null) {
-      instance = new LEDs();
+      if (RobotConfig.getInstance().getLEDHardware() == RobotConfig.LED_HARDWARE.CANDLE) {
+        instance = new LEDsCANdle();
+      } else {
+        instance = new LEDsRIO();
+      }
     }
     return instance;
   }
@@ -50,8 +49,6 @@ public class LEDs extends SubsystemBase {
   private boolean estopped = false;
 
   // LED IO
-  private final CANdle candle;
-  private final Color8Bit[] ledBuffer;
   private final Notifier loadingNotifier;
 
   // Constants
@@ -63,9 +60,9 @@ public class LEDs extends SubsystemBase {
    * before updating the LEDs.
    */
 
-  private static final boolean MIRROR_LEDS = true;
-  private static final int ACTUAL_LENGTH = RobotConfig.getInstance().getLEDCount();
-  private static final int LENGTH = MIRROR_LEDS ? ACTUAL_LENGTH / 2 : ACTUAL_LENGTH;
+  protected static final boolean MIRROR_LEDS = true;
+  protected static final int ACTUAL_LENGTH = RobotConfig.getInstance().getLEDCount();
+  protected static final int LENGTH = MIRROR_LEDS ? ACTUAL_LENGTH / 2 : ACTUAL_LENGTH;
   private static final int STATIC_LENGTH = LENGTH / 2;
   private static final int STATIC_SECTION_LENGTH = STATIC_LENGTH / 3;
   private static final boolean PRIDE_LEDS = false;
@@ -85,22 +82,7 @@ public class LEDs extends SubsystemBase {
   private static final double AUTO_FADE_TIME = 2.5; // 3s nominal
   private static final double AUTO_FADE_MAX_TIME = 5.0; // Return to normal
 
-  private LEDs() {
-    candle = new CANdle(22, RobotConfig.getInstance().getCANBusName());
-
-    CANdleConfiguration configSettings = new CANdleConfiguration();
-    configSettings.statusLedOffWhenActive = true;
-    configSettings.disableWhenLOS = false;
-    configSettings.stripType = LEDStripType.GRB;
-    configSettings.brightnessScalar = .1;
-    configSettings.vBatOutputMode = VBatOutputMode.Modulated;
-    candle.configAllSettings(configSettings, 100);
-
-    ledBuffer = new Color8Bit[ACTUAL_LENGTH];
-    for (int i = 0; i < ACTUAL_LENGTH; i++) {
-      ledBuffer[i] = convertTo8BitColor(Color.kBlack);
-    }
-
+  protected LEDs() {
     loadingNotifier =
         new Notifier(
             () -> {
@@ -118,6 +100,153 @@ public class LEDs extends SubsystemBase {
 
   @Override
   public synchronized void periodic() {
+    // update all state variables
+    updateState();
+
+    // exit during initial cycles
+    if (++loopCycleCount < MIN_LOOP_CYCLE_COUNT) {
+      return;
+    }
+
+    // stop loading notifier if running
+    loadingNotifier.stop();
+
+    // select LED mode
+    updateLEDPattern();
+
+    // Update LEDs
+    this.updateLEDs();
+  }
+
+  private void updateLEDPattern() {
+    // default to off
+    solid(Section.FULL, Color.kBlack);
+
+    if (estopped) {
+      solid(Section.FULL, Color.kRed);
+    } else if (DriverStation.isDisabled()) {
+      if (lastEnabledAuto && Timer.getFPGATimestamp() - lastEnabledTime < AUTO_FADE_MAX_TIME) {
+        // Auto fade
+        solid(1.0 - ((Timer.getFPGATimestamp() - lastEnabledTime) / AUTO_FADE_TIME), Color.kGreen);
+
+      } else if (lowBatteryAlert) {
+        // Low battery
+        solid(Section.FULL, Color.kOrangeRed);
+
+      } else if (PRIDE_LEDS) {
+        // Pride stripes
+        updateToPridePattern();
+
+      } else {
+        // Default pattern
+        updateToDisabledPattern();
+      }
+    } else if (fallen) {
+      strobe(Section.FULL, Color.kWhite, STROBE_FAST_DURATION);
+    } else if (DriverStation.isAutonomous()) {
+      updateToAutoPattern();
+    } else { // teleop
+
+      updateToTeleopPattern();
+    }
+  }
+
+  private void updateToTeleopPattern() {
+    // FIXME: add other patterns here based specific to the game
+
+    // Set special modes
+
+    // Demo mode background
+    if (demoMode) {
+      wave(
+          Section.FULL,
+          Color.kDarkOrange,
+          Color.kDarkBlue,
+          WAVE_SLOW_CYCLE_LENGTH,
+          WAVE_SLOW_DURATION);
+    }
+
+    if (distraction) {
+      strobe(Section.SHOULDER, Color.kWhite, STROBE_FAST_DURATION);
+    } else if (endgameAlert) {
+      strobe(Section.SHOULDER, Color.kBlue, STROBE_SLOW_DURATION);
+    }
+  }
+
+  private void updateToAutoPattern() {
+    wave(
+        Section.FULL,
+        Color.kDarkOrange,
+        Color.kDarkBlue,
+        WAVE_FAST_CYCLE_LENGTH,
+        WAVE_FAST_DURATION);
+    if (autoFinished) {
+      double fullTime = LENGTH / WAVE_FAST_CYCLE_LENGTH * WAVE_FAST_DURATION;
+      solid((Timer.getFPGATimestamp() - autoFinishedTime) / fullTime, Color.kGreen);
+    }
+  }
+
+  private void updateToDisabledPattern() {
+    switch (alliance) {
+      case Red:
+        wave(
+            Section.FULL,
+            Color.kRed,
+            Color.kBlack,
+            WAVE_ALLIANCE_CYCLE_LENGTH,
+            WAVE_ALLIANCE_DURATION);
+        break;
+      case Blue:
+        wave(
+            Section.FULL,
+            Color.kBlue,
+            Color.kBlack,
+            WAVE_ALLIANCE_CYCLE_LENGTH,
+            WAVE_ALLIANCE_DURATION);
+        break;
+      default:
+        wave(
+            Section.FULL,
+            Color.kDarkOrange,
+            Color.kDarkBlue,
+            WAVE_SLOW_CYCLE_LENGTH,
+            WAVE_SLOW_DURATION);
+        break;
+    }
+  }
+
+  private void updateToPridePattern() {
+    stripes(
+        Section.FULL,
+        List.of(
+            Color.kBlack,
+            Color.kRed,
+            Color.kOrangeRed,
+            Color.kYellow,
+            Color.kGreen,
+            Color.kBlue,
+            Color.kPurple,
+            Color.kBlack,
+            new Color(0.15, 0.3, 1.0),
+            Color.kDeepPink,
+            Color.kWhite,
+            Color.kDeepPink,
+            new Color(0.15, 0.3, 1.0)),
+        3,
+        5.0);
+    switch (alliance) {
+      case Red:
+        solid(Section.STATIC_LOW, Color.kRed);
+        break;
+      case Blue:
+        solid(Section.STATIC_LOW, Color.kBlue);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private void updateState() {
     // Update alliance color
     if (DriverStation.isFMSAttached()) {
       alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
@@ -135,127 +264,6 @@ public class LEDs extends SubsystemBase {
     if (DriverStation.isEStopped()) {
       estopped = true;
     }
-
-    // Exit during initial cycles
-    loopCycleCount += 1;
-    if (loopCycleCount < MIN_LOOP_CYCLE_COUNT) {
-      return;
-    }
-
-    // Stop loading notifier if running
-    loadingNotifier.stop();
-
-    // Select LED mode
-    solid(Section.FULL, Color.kBlack); // Default to off
-    if (estopped) {
-      solid(Section.FULL, Color.kRed);
-    } else if (DriverStation.isDisabled()) {
-      if (lastEnabledAuto && Timer.getFPGATimestamp() - lastEnabledTime < AUTO_FADE_MAX_TIME) {
-        // Auto fade
-        solid(1.0 - ((Timer.getFPGATimestamp() - lastEnabledTime) / AUTO_FADE_TIME), Color.kGreen);
-
-      } else if (lowBatteryAlert) {
-        // Low battery
-        solid(Section.FULL, Color.kOrangeRed);
-
-      } else if (PRIDE_LEDS) {
-        // Pride stripes
-        stripes(
-            Section.FULL,
-            List.of(
-                Color.kBlack,
-                Color.kRed,
-                Color.kOrangeRed,
-                Color.kYellow,
-                Color.kGreen,
-                Color.kBlue,
-                Color.kPurple,
-                Color.kBlack,
-                new Color(0.15, 0.3, 1.0),
-                Color.kDeepPink,
-                Color.kWhite,
-                Color.kDeepPink,
-                new Color(0.15, 0.3, 1.0)),
-            3,
-            5.0);
-        switch (alliance) {
-          case Red:
-            solid(Section.STATIC_LOW, Color.kRed);
-            break;
-          case Blue:
-            solid(Section.STATIC_LOW, Color.kBlue);
-            break;
-          default:
-            break;
-        }
-
-      } else {
-        // Default pattern
-        switch (alliance) {
-          case Red:
-            wave(
-                Section.FULL,
-                Color.kRed,
-                Color.kBlack,
-                WAVE_ALLIANCE_CYCLE_LENGTH,
-                WAVE_ALLIANCE_DURATION);
-            break;
-          case Blue:
-            wave(
-                Section.FULL,
-                Color.kBlue,
-                Color.kBlack,
-                WAVE_ALLIANCE_CYCLE_LENGTH,
-                WAVE_ALLIANCE_DURATION);
-            break;
-          default:
-            wave(
-                Section.FULL,
-                Color.kDarkOrange,
-                Color.kDarkBlue,
-                WAVE_SLOW_CYCLE_LENGTH,
-                WAVE_SLOW_DURATION);
-            break;
-        }
-      }
-    } else if (fallen) {
-      strobe(Section.FULL, Color.kWhite, STROBE_FAST_DURATION);
-    } else if (DriverStation.isAutonomous()) {
-      wave(
-          Section.FULL,
-          Color.kDarkOrange,
-          Color.kDarkBlue,
-          WAVE_FAST_CYCLE_LENGTH,
-          WAVE_FAST_DURATION);
-      if (autoFinished) {
-        double fullTime = LENGTH / WAVE_FAST_CYCLE_LENGTH * WAVE_FAST_DURATION;
-        solid((Timer.getFPGATimestamp() - autoFinishedTime) / fullTime, Color.kGreen);
-      }
-    } else {
-
-      // FIXME: add other patterns here based specific to the game
-
-      // Set special modes
-
-      // Demo mode background
-      if (demoMode) {
-        wave(
-            Section.FULL,
-            Color.kDarkOrange,
-            Color.kDarkBlue,
-            WAVE_SLOW_CYCLE_LENGTH,
-            WAVE_SLOW_DURATION);
-      }
-
-      if (distraction) {
-        strobe(Section.SHOULDER, Color.kWhite, STROBE_FAST_DURATION);
-      } else if (endgameAlert) {
-        strobe(Section.SHOULDER, Color.kBlue, STROBE_SLOW_DURATION);
-      }
-    }
-
-    // Update LEDs
-    this.updateLEDs();
   }
 
   public void setDistraction(boolean distraction) {
@@ -285,27 +293,14 @@ public class LEDs extends SubsystemBase {
     this.demoMode = demoMode;
   }
 
-  private void updateLEDs() {
-    // mirror buffer
-    if (MIRROR_LEDS) {
-      for (int i = 0; i < LENGTH; i++) {
-        ledBuffer[ACTUAL_LENGTH - i - 1] = ledBuffer[i];
-      }
-    }
+  protected abstract void updateLEDs();
 
-    for (int i = 0; i < ACTUAL_LENGTH; i++) {
-      int count = 1;
-      while (i + count < ACTUAL_LENGTH && ledBuffer[i] == ledBuffer[i + count]) {
-        count++;
-      }
-      candle.setLEDs(ledBuffer[i].red, ledBuffer[i].green, ledBuffer[i].blue, 0, i, count);
-    }
-  }
+  protected abstract void setLEDBuffer(int index, Color color);
 
   private void solid(Section section, Color color) {
     if (color != null) {
       for (int i = section.start(); i < section.end(); i++) {
-        ledBuffer[i] = convertTo8BitColor(color);
+        setLEDBuffer(i, color);
       }
     }
   }
@@ -313,7 +308,7 @@ public class LEDs extends SubsystemBase {
   private void solid(double percent, Color color) {
     if (color != null) {
       for (int i = 0; i < MathUtil.clamp(LENGTH * percent, 0, LENGTH); i++) {
-        ledBuffer[i] = convertTo8BitColor(color);
+        setLEDBuffer(i, color);
       }
     }
   }
@@ -343,7 +338,7 @@ public class LEDs extends SubsystemBase {
       x += xDiffPerLed;
       x %= 180.0;
       if (i >= section.start()) {
-        ledBuffer[i] = convertTo8BitColor(Color.fromHSV((int) x, 255, 255));
+        setLEDBuffer(i, Color.fromHSV((int) x, 255, 255));
       }
     }
   }
@@ -364,7 +359,7 @@ public class LEDs extends SubsystemBase {
         double red = (c1.red * (1 - ratio)) + (c2.red * ratio);
         double green = (c1.green * (1 - ratio)) + (c2.green * ratio);
         double blue = (c1.blue * (1 - ratio)) + (c2.blue * ratio);
-        ledBuffer[i] = convertTo8BitColor(new Color(red, green, blue));
+        setLEDBuffer(i, new Color(red, green, blue));
       }
     }
   }
@@ -375,7 +370,7 @@ public class LEDs extends SubsystemBase {
       int colorIndex =
           (int) (Math.floor((double) (i - offset) / length) + colors.size()) % colors.size();
       colorIndex = colors.size() - 1 - colorIndex;
-      ledBuffer[i] = convertTo8BitColor(colors.get(colorIndex));
+      setLEDBuffer(i, colors.get(colorIndex));
     }
   }
 
@@ -424,10 +419,5 @@ public class LEDs extends SubsystemBase {
           return LENGTH;
       }
     }
-  }
-
-  private static Color8Bit convertTo8BitColor(Color color) {
-    return new Color8Bit(
-        (int) (color.red * 255), (int) (color.green * 255), (int) (color.blue * 255));
   }
 }
