f32da04a161a53b18a03a08dd225c8f5cc7264cb
tyler51 <77416087+tyler51@users.noreply.github.com>
2024-01-25 09:52:14 -0600

    Offsets for practice robot (#93)

--- src/main/java/frc/robot/configs/PracticeRobotConfig.java
+++ src/main/java/frc/robot/configs/PracticeRobotConfig.java
@@ -16,22 +16,22 @@ public class PracticeRobotConfig extends RobotConfig {
   private static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 13;
   private static final int FRONT_LEFT_MODULE_STEER_MOTOR = 12;
   private static final int FRONT_LEFT_MODULE_STEER_ENCODER = 14;
-  private static final double FRONT_LEFT_MODULE_STEER_OFFSET_ROT = -0.22591;
+  private static final double FRONT_LEFT_MODULE_STEER_OFFSET_ROT = -0.013428 + 0.5;
 
   private static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 16;
   private static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 15;
   private static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 17;
-  private static final double FRONT_RIGHT_MODULE_STEER_OFFSET_ROT = -0.390381;
+  private static final double FRONT_RIGHT_MODULE_STEER_OFFSET_ROT = -0.342773;
 
   private static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
   private static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
   private static final int BACK_LEFT_MODULE_STEER_ENCODER = 8;
-  private static final double BACK_LEFT_MODULE_STEER_OFFSET_ROT = 0.327393;
+  private static final double BACK_LEFT_MODULE_STEER_OFFSET_ROT = 0.263184 + 0.5;
 
   private static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10;
   private static final int BACK_RIGHT_MODULE_STEER_MOTOR = 9;
   private static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-  private static final double BACK_RIGHT_MODULE_STEER_OFFSET_ROT = -0.336670;
+  private static final double BACK_RIGHT_MODULE_STEER_OFFSET_ROT = -0.024658;
 
   private static final int GYRO_ID = 3;
 
