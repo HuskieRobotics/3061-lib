b7a6d8d276769190adbe4f508c0aeea4d8a01bd8
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2025-01-09 17:34:44 -0800

    update offsets and ids

--- src/main/java/frc/robot/configs/NewPracticeRobotConfig.java
+++ src/main/java/frc/robot/configs/NewPracticeRobotConfig.java
@@ -19,22 +19,22 @@ public class NewPracticeRobotConfig extends RobotConfig {
   private static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 43;
   private static final int FRONT_LEFT_MODULE_STEER_MOTOR = 44;
   private static final int FRONT_LEFT_MODULE_STEER_ENCODER = 14;
-  private static final Angle FRONT_LEFT_MODULE_STEER_OFFSET = Rotations.of(0.474121); // tune
+  private static final Angle FRONT_LEFT_MODULE_STEER_OFFSET = Rotations.of(0.271484);
 
   private static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 41;
   private static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 42;
-  private static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 17;
-  private static final Angle FRONT_RIGHT_MODULE_STEER_OFFSET = Rotations.of(-0.358398); // tune
+  private static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 8;
+  private static final Angle FRONT_RIGHT_MODULE_STEER_OFFSET = Rotations.of(-0.487793);
 
   private static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 38;
   private static final int BACK_LEFT_MODULE_STEER_MOTOR = 37;
-  private static final int BACK_LEFT_MODULE_STEER_ENCODER = 8;
-  private static final Angle BACK_LEFT_MODULE_STEER_OFFSET = Rotations.of(-0.234863); // tune
+  private static final int BACK_LEFT_MODULE_STEER_ENCODER = 17;
+  private static final Angle BACK_LEFT_MODULE_STEER_OFFSET = Rotations.of(-0.280029);
 
   private static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 39;
   private static final int BACK_RIGHT_MODULE_STEER_MOTOR = 40;
   private static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-  private static final Angle BACK_RIGHT_MODULE_STEER_OFFSET = Rotations.of(0.161133); // tune
+  private static final Angle BACK_RIGHT_MODULE_STEER_OFFSET = Rotations.of(-0.105957);
 
   private static final int GYRO_ID = 3; // ?
 
