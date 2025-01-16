0dc698194489d114c4006d3db8a97b011ae278fc
Geoff Schmit <gcschmit@gmail.com>
2025-01-16 13:41:04 -0600

    Switch to 2025 Reefscape AprilTag field layout.

--- src/main/java/frc/lib/team3061/vision/VisionConstants.java
+++ src/main/java/frc/lib/team3061/vision/VisionConstants.java
@@ -12,10 +12,10 @@ public final class VisionConstants {
   }
 
   public static final Path APRILTAG_FIELD_LAYOUT_PATH =
-      new File(Filesystem.getDeployDirectory(), "home.json").toPath();
+      new File(Filesystem.getDeployDirectory(), "2025-reefscape.json").toPath();
   // FIXME: update for the new season's file
   public static final Path OFFICIAL_APRILTAG_FIELD_LAYOUT_PATH =
-      new File(Filesystem.getDeployDirectory(), "2024-crescendo.json").toPath();
+      new File(Filesystem.getDeployDirectory(), "2025-reefscape.json").toPath();
   public static final int MAX_NUMBER_TAGS = 30;
 
   public static final String SUBSYSTEM_NAME = "Vision";
