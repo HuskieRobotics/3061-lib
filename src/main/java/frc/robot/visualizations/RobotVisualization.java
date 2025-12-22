74c95b4a07058e86c2c871aa33c93c6c0bf2dc0a
Geoff Schmit <gcschmit@gmail.com>
2025-12-21 20:15:55 -0600

    Add two-stage elevator as Mechanism2D.

--- src/main/java/frc/robot/visualizations/RobotVisualization.java
+++ src/main/java/frc/robot/visualizations/RobotVisualization.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.lib.team3061.RobotConfig;
 import frc.lib.team3061.leds.LEDs;
 import frc.robot.subsystems.elevator.Elevator;
 import org.littletonrobotics.junction.Logger;
@@ -20,32 +21,19 @@ public class RobotVisualization {
 
   private Elevator elevator;
 
-  private LoggedMechanism2d visualization2D = new LoggedMechanism2d(1.27, 2.032);
+  private LoggedMechanism2d visualization2D;
 
-  private LoggedMechanismRoot2d ledDisplayRoot =
-      visualization2D.getRoot("ledDisplay", 0.58, 1.5); // this goes above the robot
-  private LoggedMechanismLigament2d ledDisplayBox =
-      new LoggedMechanismLigament2d("LED State", 0.5, 0, 5.0, new Color8Bit(Color.kBlack));
-
-  private Color8Bit blue = new Color8Bit(Color.kBlue);
-  private Color8Bit white = new Color8Bit(Color.kWhite);
-
-  private LoggedMechanismRoot2d driveRoot = visualization2D.getRoot("drive", 0.25, 0);
-  private LoggedMechanismLigament2d driveLigament =
-      new LoggedMechanismLigament2d("drivetrain", 0.76, 0.0, 5.0, blue);
+  private LoggedMechanismRoot2d ledDisplayRoot;
+  private LoggedMechanismLigament2d ledDisplayBox;
 
   private final double kElevatorPosX = 0.75;
   private final double kElevatorPosY = 0.51;
-  private LoggedMechanismRoot2d elevatorRoot =
-      visualization2D.getRoot("elevatorRoot", kElevatorPosX, kElevatorPosY);
+  private final double kElevatorStageLength = 0.5;
+  private LoggedMechanismRoot2d elevatorRoot;
 
-  private LoggedMechanismLigament2d elevatorLigament =
-      new LoggedMechanismLigament2d("elevator", 0.13, 90.0, 20.0, white);
-
-  private Pose3d elevatorBasePose3d = new Pose3d();
-  private Pose3d elevatorMiddlePose3d = new Pose3d();
-  private Pose3d elevatorTopPose3d = new Pose3d();
-  private Color8Bit ledColor = new Color8Bit(0, 0, 0);
+  private LoggedMechanismLigament2d elevatorStage1;
+  private LoggedMechanismLigament2d elevatorStage2;
+  private LoggedMechanismLigament2d elevatorStage3;
 
   public RobotVisualization(Elevator elevator) {
     this.elevator = elevator;
@@ -54,53 +42,69 @@ public class RobotVisualization {
   }
 
   private void init2dVisualization() {
-    visualization2D = new LoggedMechanism2d(1.27, 2.032);
+    visualization2D =
+        new LoggedMechanism2d(
+            RobotConfig.getInstance().getRobotLengthWithBumpers().in(Meters), 3.0);
 
-    ledDisplayRoot = visualization2D.getRoot("ledDisplay", 0.58, 1.5); // this goes above the robot
+    ledDisplayRoot =
+        visualization2D.getRoot(
+            "ledDisplay",
+            RobotConfig.getInstance().getRobotLengthWithBumpers().in(Meters) / 2.0,
+            2.0); // this goes above the robot
     ledDisplayBox =
         new LoggedMechanismLigament2d("LED State", 0.5, 0, 5.0, new Color8Bit(Color.kBlack));
-
-    driveRoot = visualization2D.getRoot("drive", 0.25, 0);
-    driveLigament = new LoggedMechanismLigament2d("drivetrain", 0.76, 0.0, 5.0, blue);
-
-    elevatorRoot = visualization2D.getRoot("elevatorRoot", kElevatorPosX, kElevatorPosY);
-
-    elevatorLigament = new LoggedMechanismLigament2d("elevator", 0.13, 90.0, 20.0, white);
-
     ledDisplayRoot.append(ledDisplayBox);
-    driveRoot.append(this.driveLigament);
 
-    // Set up elevator and rollers
-    elevatorRoot.append(this.elevatorLigament);
+    elevatorRoot =
+        visualization2D.getRoot(
+            "elevatorRoot",
+            RobotConfig.getInstance().getRobotLengthWithBumpers().in(Meters) / 2.0,
+            kElevatorPosY);
+    elevatorStage1 =
+        new LoggedMechanismLigament2d("stage1", 0.0, 90.0, 20.0, new Color8Bit(Color.kWhite));
+    elevatorStage2 =
+        new LoggedMechanismLigament2d("stage2", 0.0, 0.0, 20.0, new Color8Bit(Color.kBlue));
+    elevatorStage3 =
+        new LoggedMechanismLigament2d("stage3", 0.0, 0.0, 20.0, new Color8Bit(Color.kRed));
+    elevatorRoot.append(this.elevatorStage1);
+    elevatorStage1.append(this.elevatorStage2);
+    elevatorStage2.append(this.elevatorStage3);
   }
 
   public void update() {
+    if (RobotBase.isReal()) return;
+
+    ledDisplayBox.setColor(LEDs.getInstance().getColor(0));
+
+    double elevatorHeightM = this.elevator.getPosition().in(Meters);
+    double remainingHeightM = elevatorHeightM;
+
+    // Update elevator stages based on height
+    double stage1Length = Math.min(kElevatorStageLength, remainingHeightM);
+    remainingHeightM -= stage1Length;
+    double stage2Length = Math.min(kElevatorStageLength, remainingHeightM);
+    remainingHeightM -= stage2Length;
+    double stage3Length = remainingHeightM;
+
+    this.elevatorStage1.setLength(stage1Length);
+    this.elevatorStage2.setLength(stage2Length);
+    this.elevatorStage3.setLength(stage3Length);
+
+    Logger.recordOutput("Visualization/Mechanisms2D", this.visualization2D);
+
     // model_0 is elevator bottom cascade
     // model_1 is elevator middle cascade
     // model_2 is elevator top cascade
-    double elevatorHeightM = this.elevator.getPosition().in(Meters);
-
-    // Update elevator stages based on height
     double baseHeight = 0.0;
     double middleHeight = Math.max(0.0, elevatorHeightM - 0.631825);
     double topHeight = elevatorHeightM;
 
-    elevatorBasePose3d = new Pose3d(0, 0, baseHeight, new Rotation3d(0, 0, 0));
-    elevatorMiddlePose3d = new Pose3d(0, 0, middleHeight, new Rotation3d(0, 0, 0));
-    elevatorTopPose3d = new Pose3d(0, 0, topHeight, new Rotation3d(0, 0, 0));
+    Pose3d elevatorBasePose3d = new Pose3d(0, 0, baseHeight, new Rotation3d(0, 0, 0));
+    Pose3d elevatorMiddlePose3d = new Pose3d(0, 0, middleHeight, new Rotation3d(0, 0, 0));
+    Pose3d elevatorTopPose3d = new Pose3d(0, 0, topHeight, new Rotation3d(0, 0, 0));
 
     Logger.recordOutput(
         "Visualization/ComponentsPoseArray",
         new Pose3d[] {elevatorBasePose3d, elevatorMiddlePose3d, elevatorTopPose3d});
-
-    ledColor = LEDs.getInstance().getColor(0);
-
-    if (RobotBase.isReal()) return;
-
-    this.elevatorRoot.setPosition(kElevatorPosX, kElevatorPosY + elevatorHeightM);
-
-    Logger.recordOutput("Visualization/Mechanisms2D", this.visualization2D);
-
-    ledDisplayBox.setColor(ledColor);
   }
 }
