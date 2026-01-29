6a14fa329a8030048543d78469fa0e5892770fcb
Kush Shah <kmshah2@stu.naperville203.org>
2026-01-28 18:41:33 -0800

    Camera from 1/28/26 today

--- src/main/java/frc/robot/configs/NewPracticeRobotConfig.java
+++ src/main/java/frc/robot/configs/NewPracticeRobotConfig.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.MountPoseConfigs;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import edu.wpi.first.math.geometry.Quaternion;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -111,10 +112,14 @@ public class NewPracticeRobotConfig extends RobotConfig {
   private static final String CAMERA_NAME_1 = "40708569";
   private static final String CAMERA_NAME_2 = "40708556";
   private static final String CAMERA_NAME_3 = "40708542";
+  private static final String CAMERA_NAME_4 = "25249734";
 
   private static final int MONO_EXPOSURE = 2200;
   private static final double MONO_GAIN = 17.5;
   private static final double MONO_DENOISE = 1.0;
+
+  private static final int COLOR_EXPOSURE = 4500;
+  private static final double COLOR_GAIN = 5.0;
 
   // Front right camera
   private static final Transform3d ROBOT_TO_CAMERA_0 =
@@ -153,6 +158,11 @@ public class NewPracticeRobotConfig extends RobotConfig {
               Units.inchesToMeters(10.778),
               Units.inchesToMeters(8.052)),
           new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(180)));
+
+  // color camera
+  private static final Transform3d ROBOT_TO_CAMERA_4 =
+      new Transform3d(
+          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
 
   @Override
   public CameraConfig[] getCameraConfigs() {
@@ -199,6 +209,16 @@ public class NewPracticeRobotConfig extends RobotConfig {
           .exposure(MONO_EXPOSURE)
           .gain(MONO_GAIN)
           .denoise(MONO_DENOISE)
+          .stdDevFactor(1.0)
+          .build(),
+      CameraConfig.builder()
+          .robotToCameraTransform(ROBOT_TO_CAMERA_4)
+          .id(CAMERA_NAME_4)
+          .location("center")
+          .width(1280)
+          .height(960)
+          .exposure(COLOR_EXPOSURE)
+          .gain(COLOR_GAIN)
           .stdDevFactor(1.0)
           .build(),
     };
