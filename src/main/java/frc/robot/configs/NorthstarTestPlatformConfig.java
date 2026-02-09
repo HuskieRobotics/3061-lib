95b5405cf0634705db6bd3b1fc1ff694a035eb89
Geoff Schmit <gcschmit@gmail.com>
2026-02-08 18:35:44 -0600

    use a single camera to debug northstar

--- src/main/java/frc/robot/configs/NorthstarTestPlatformConfig.java
+++ src/main/java/frc/robot/configs/NorthstarTestPlatformConfig.java
@@ -17,11 +17,7 @@ import frc.lib.team3061.swerve_drivetrain.swerve.SwerveConstants;
  * these methods, refer to the documentation in the RobotConfig class.
  */
 public class NorthstarTestPlatformConfig extends RobotConfig {
-  private static final String CAMERA_NAME_0 = "40686739";
-  private static final String CAMERA_NAME_1 = "40708569";
-  private static final String CAMERA_NAME_2 = "40708556";
-  private static final String CAMERA_NAME_3 = "40708542";
-  private static final String CAMERA_NAME_4 = "25249734";
+  private static final String CAMERA_NAME = "40708569";
 
   private static final int MONO_EXPOSURE = 2200;
   private static final double MONO_GAIN = 17.5;
@@ -30,28 +26,8 @@ public class NorthstarTestPlatformConfig extends RobotConfig {
   private static final int COLOR_EXPOSURE = 4500;
   private static final double COLOR_GAIN = 5.0;
 
-  // Front right camera
-  private static final Transform3d ROBOT_TO_CAMERA_0 =
-      new Transform3d(
-          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
-
-  // Back right camera
-  private static final Transform3d ROBOT_TO_CAMERA_1 =
-      new Transform3d(
-          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
-
-  // Back right camera
-  private static final Transform3d ROBOT_TO_CAMERA_2 =
-      new Transform3d(
-          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
-
-  // Back right camera
-  private static final Transform3d ROBOT_TO_CAMERA_3 =
-      new Transform3d(
-          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
-
-  // color camera
-  private static final Transform3d ROBOT_TO_CAMERA_4 =
+  // "BL" camera
+  private static final Transform3d ROBOT_TO_CAMERA =
       new Transform3d(
           new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
 
@@ -64,57 +40,14 @@ public class NorthstarTestPlatformConfig extends RobotConfig {
   public CameraConfig[] getCameraConfigs() {
     return new CameraConfig[] {
       CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA_0)
-          .id(CAMERA_NAME_0)
-          .location("FR")
-          .width(1920)
-          .height(1200)
-          .exposure(MONO_EXPOSURE)
-          .gain(MONO_GAIN)
-          .denoise(MONO_DENOISE)
-          .stdDevFactor(1.0)
-          .build(),
-      CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA_1)
-          .id(CAMERA_NAME_1)
-          .location("BR")
-          .width(1920)
-          .height(1200)
-          .exposure(MONO_EXPOSURE)
-          .gain(MONO_GAIN)
-          .denoise(MONO_DENOISE)
-          .stdDevFactor(1.0)
-          .build(),
-      CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA_2)
-          .id(CAMERA_NAME_2)
-          .location("FL")
-          .width(1920)
-          .height(1200)
-          .exposure(MONO_EXPOSURE)
-          .gain(MONO_GAIN)
-          .denoise(MONO_DENOISE)
-          .stdDevFactor(1.0)
-          .build(),
-      CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA_3)
-          .id(CAMERA_NAME_3)
+          .robotToCameraTransform(ROBOT_TO_CAMERA)
+          .id(CAMERA_NAME)
           .location("BL")
           .width(1920)
           .height(1200)
           .exposure(MONO_EXPOSURE)
           .gain(MONO_GAIN)
           .denoise(MONO_DENOISE)
-          .stdDevFactor(1.0)
-          .build(),
-      CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA_4)
-          .id(CAMERA_NAME_4)
-          .location("center")
-          .width(1280)
-          .height(960)
-          .exposure(COLOR_EXPOSURE)
-          .gain(COLOR_GAIN)
           .stdDevFactor(1.0)
           .build(),
     };
