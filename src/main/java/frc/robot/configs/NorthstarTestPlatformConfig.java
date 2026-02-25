14f8c8033be508adc1b5a8051adba7e3385f9532
Geoff Schmit <gcschmit@gmail.com>
2026-02-25 15:59:24 -0600

    update for comp bot

--- src/main/java/frc/robot/configs/NorthstarTestPlatformConfig.java
+++ src/main/java/frc/robot/configs/NorthstarTestPlatformConfig.java
@@ -2,10 +2,10 @@ package frc.robot.configs;
 
 import static edu.wpi.first.units.Units.*;
 
-import edu.wpi.first.math.geometry.Quaternion;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
@@ -17,7 +17,9 @@ import frc.lib.team3061.swerve_drivetrain.swerve.SwerveConstants;
  * these methods, refer to the documentation in the RobotConfig class.
  */
 public class NorthstarTestPlatformConfig extends RobotConfig {
-  private static final String CAMERA_NAME = "40708569";
+  private static final String BR_CAMERA_SERIAL_NUMBER = "40686739";
+  private static final String BL_CAMERA_SERIAL_NUMBER = "40708556";
+  private static final String CENTER_CAMERA_SERIAL_NUMBER = "25249734";
 
   private static final int MONO_EXPOSURE = 2200;
   private static final double MONO_GAIN = 17.5;
@@ -26,22 +28,51 @@ public class NorthstarTestPlatformConfig extends RobotConfig {
   private static final int COLOR_EXPOSURE = 4500;
   private static final double COLOR_GAIN = 5.0;
 
-  // "BL" camera
-  private static final Transform3d ROBOT_TO_CAMERA =
+  // Back right camera
+  // x, y, z, pitch, yaw
+  private static final Transform3d ROBOT_TO_BR_CAMERA =
       new Transform3d(
-          new Translation3d(0.0, 0.0, 0.0), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 0.0)));
+          new Translation3d(
+              Units.inchesToMeters(-8.969),
+              Units.inchesToMeters(-11.729),
+              Units.inchesToMeters(7.434)),
+          new Rotation3d(0, Units.degreesToRadians(-25), Units.degreesToRadians(-90.0)));
 
-  @Override
-  public boolean getPhoenix6Licensed() {
-    return true;
-  }
+  // Back left camera
+  // x, y, z, pitch, yaw
+  private static final Transform3d ROBOT_TO_BL_CAMERA =
+      new Transform3d(
+          new Translation3d(
+              Units.inchesToMeters(-8.969),
+              Units.inchesToMeters(11.729),
+              Units.inchesToMeters(7.434)),
+          new Rotation3d(0, Units.degreesToRadians(-25), Units.degreesToRadians(90.0)));
+
+  // Center camera
+  // x, y, z, pitch, yaw
+  private static final Transform3d ROBOT_TO_CENTER_CAMERA =
+      new Transform3d(
+          new Translation3d(
+              Units.inchesToMeters(0.0), Units.inchesToMeters(0.0), Units.inchesToMeters(0.0)),
+          new Rotation3d(0, Units.degreesToRadians(0.0), Units.degreesToRadians(0.0)));
 
   @Override
   public CameraConfig[] getCameraConfigs() {
     return new CameraConfig[] {
       CameraConfig.builder()
-          .robotToCameraTransform(ROBOT_TO_CAMERA)
-          .id(CAMERA_NAME)
+          .robotToCameraTransform(ROBOT_TO_BR_CAMERA)
+          .id(BR_CAMERA_SERIAL_NUMBER)
+          .location("BR")
+          .width(1920)
+          .height(1200)
+          .exposure(MONO_EXPOSURE)
+          .gain(MONO_GAIN)
+          .denoise(MONO_DENOISE)
+          .stdDevFactor(1.0)
+          .build(),
+      CameraConfig.builder()
+          .robotToCameraTransform(ROBOT_TO_BL_CAMERA)
+          .id(BL_CAMERA_SERIAL_NUMBER)
           .location("BL")
           .width(1920)
           .height(1200)
@@ -50,7 +81,22 @@ public class NorthstarTestPlatformConfig extends RobotConfig {
           .denoise(MONO_DENOISE)
           .stdDevFactor(1.0)
           .build(),
+      CameraConfig.builder()
+          .robotToCameraTransform(ROBOT_TO_CENTER_CAMERA)
+          .id(CENTER_CAMERA_SERIAL_NUMBER)
+          .location("center")
+          .width(1280)
+          .height(960)
+          .exposure(COLOR_EXPOSURE)
+          .gain(COLOR_GAIN)
+          .stdDevFactor(1.0)
+          .build(),
     };
+  }
+
+  @Override
+  public boolean getPhoenix6Licensed() {
+    return true;
   }
 
   @Override
